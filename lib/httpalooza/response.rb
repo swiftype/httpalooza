df9c5eb404caaa2cae773bcf39f12836c07f1f72
Brian Stevenson <bstevenson@swiftype.com>
2016-03-25 17:59:28 -0700

    Test all the players against each other

--- lib/httpalooza/response.rb
+++ lib/httpalooza/response.rb
@@ -20,5 +20,9 @@ module HTTPalooza
     def inspect
       "<HTTPalooza::Response:#{object_id} code=#{code} body=#{body.to_s.inspect.truncate(30)}"
     end
+
+    def as_key
+      "code=#{@code} body=#{@body}"
+    end
   end
 end
--- spec/httpalooza_spec.rb
+++ spec/httpalooza_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'set'
 
 describe HTTPalooza do
   it 'has a version number' do
@@ -8,5 +9,22 @@ describe HTTPalooza do
   it 'should be hilarious' do
     hilarious = true
     expect(hilarious).to eq(true)
+  end
+
+  it 'can make a request for everyone' do
+    responses = HTTPalooza.invite.everyone.except_user_browser_lawl.get("http://httpbin.org/")
+    unique_responses = {}
+    non_awesome_players = {}
+    responses.each do |player_name, response|
+      resp_key = response.as_key
+      if unique_responses.keys.include?(resp_key)
+        unique_responses[resp_key] << player_name
+      else
+        unique_responses[resp_key] = [ player_name ]
+      end
+      non_awesome_players[player_name] = response unless response.awesome?
+    end
+    expect(unique_responses.count).to eq(1), "Not all players got the same response! #{unique_responses.values}"
+    expect(non_awesome_players).to eq({}), "Some players were not awesome! #{non_awesome_players}"
   end
 end
