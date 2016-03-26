896475d17685502d437ebbdd2dd1e175b646499d
David Harsha <dharsha@swiftype.com>
2016-03-25 17:04:07 -0700

    Add em_http_request player

---
+++ lib/httpalooza/players/em_http_request.rb
@@ -0,0 +1,18 @@
+module HTTPalooza
+  module Players
+    class EmHttpRequestPlayer < Base
+      introducing! :em_http_request, %w[ em-synchrony em-synchrony/em-http ]
+
+      def response
+        response = nil
+        EM.synchrony do
+          http_request = EventMachine::HttpRequest.new(request.url)
+          http_client = http_request.send(request.method, :query => request.params, :body => request.payload, :head => request.headers)
+          response = Response.new(http_client.response_header.status, http_client.response)
+          EM.stop
+        end
+        response
+      end
+    end
+  end
+end
